a5920ff4b6d972b30cf2ad145ac95e9edd372724
Tashdik <s2110477153@ru.ac.bd>
2024-04-08 15:00:16 +0600

    view property is animated now!!

--- src/pages/details/DetailCard.jsx
+++ src/pages/details/DetailCard.jsx
@@ -8,6 +8,7 @@ import { MdOutlineBathtub } from "react-icons/md";
 import { MdBalcony } from "react-icons/md";
 import { FaBuildingUser } from "react-icons/fa6";
 import { FaCalendarAlt } from "react-icons/fa";
+import 'animate.css';
 
 
 const DetailCard = () => {
@@ -26,15 +27,15 @@ const DetailCard = () => {
 
     return (
         <div className="p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-100 dark:text-gray-800">
-            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
-                <img src={img} alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500" />
-                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-50">
+            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded ">
+                <img src={img} alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500 animate__animated animate__zoomIn" />
+                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-50 animate__animated animate__fadeInRight">
                     <div className='flex justify-between p-2 bg-black text-white font-bold'>
                         <h1 className='flex gap-2 items-center'><FaTags />{segment_name}</h1>
                         <h5 className='flex gap-2 items-center'><FaCalendarAlt />Build in {building_year}</h5>
                         <h3 className='text-orange-700'>{price}</h3>
                     </div>
-                    <div className="space-y-2">
+                    <div className="space-y-2 animate__animated animate__fadeInRight">
                         <a rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl">{estate_title}</a>
                         <h5 className='flex gap-2 items-center text-lg font-semibold'><FaBuildingUser className='text-2xl' />{owner}</h5>
                         <p className="text-lg dark:text-gray-600 flex gap-2 items-center"><FaLocationDot />
@@ -43,13 +44,13 @@ const DetailCard = () => {
                     </div>
                     <div>
                         <h1 className='text-xl text-start font-bold underline'>Facilities</h1>
-                        <div className='text-start'>
+                        <div className='text-start animate__animated animate__fadeInRight'>
                             {
                                 facilities.map(i => <li key={i}>{i}</li>)
                             }
                         </div>
                     </div>
-                    <div className="dark:text-gray-800">
+                    <div className="dark:text-gray-800 animate__animated animate__fadeInRight">
                         <p>{description}</p>
                     </div>
                     <div className='flex justify-between bg-gray-400/25 p-2'>
