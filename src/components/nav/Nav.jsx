220fd1ab8321f02120eaa86cd52663ee77dfff8e
Tashdik <s2110477153@ru.ac.bd>
2024-04-10 16:32:25 +0600

    email field added to update profile

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -44,7 +44,7 @@ const Nav = () => {
                 {
                     user ?
                         <div className="flex items-center gap-2">
-                            <div className="w-14 rounded-full hover:tooltip tooltip-open hover:tooltip-bottom hover:z-10" data-tip={user.displayName ? user.displayName : 'User name not found'}>
+                            <div className=" border border-green-500 p-1 w-14 rounded-full hover:tooltip tooltip-open hover:tooltip-bottom hover:z-10" data-tip={user.displayName ? user.displayName : 'User name not found'}>
                                 <img className="w-full rounded-full" src={user.photoURL ? user.photoURL : placeholderImg} alt="" />
                             </div>
                             <Link onClick={handelLogout} to='/login' className="btn">Logout</Link>
--- src/pages/update/UpdateProfile.jsx
+++ src/pages/update/UpdateProfile.jsx
@@ -5,37 +5,37 @@ import auth from "../../firebase/firebase.config";
 
 const UpdateProfile = () => {
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
 
-   const [name, setName] = useState(user.displayName);
-   const [photo, setPhoto] = useState(user.photoURL);
+    const [name, setName] = useState(user.displayName);
+    const [photo, setPhoto] = useState(user.photoURL);
 
 
 
-      const handleNameChange = (e) => {
+    const handleNameChange = (e) => {
         setName(e.target.value);
-      };
+    };
 
-      const handlePhotoChange = (e) => {
+    const handlePhotoChange = (e) => {
         setPhoto(e.target.value);
-      };
+    };
 
 
-    const handelUpdate = (e) =>{
+    const handelUpdate = (e) => {
         e.preventDefault();
 
 
         updateProfile(auth.currentUser, {
-            displayName: name, 
+            displayName: name,
             photoURL: photo
-          })
-          .then(() => {
-            alert('Updated')
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        })
+            .then(() => {
+                alert('Updated')
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     return (
@@ -48,13 +48,21 @@ const UpdateProfile = () => {
                             <label className="label">
                                 <span className="label-text text-lg text-black font-bold">Username</span>
                             </label>
-                            <input type="text" name="name" value={name} onChange={handleNameChange}  className="input input-bordered" required />
+                            <input type="text" name="name" value={name} onChange={handleNameChange} className="input input-bordered" required />
+                        </div>
+                        <div className="form-control " >
+                            <label className="label">
+                                <span className="label-text text-lg text-black font-bold">Email</span>
+                            </label>
+                            <div className="hover:tooltip" data-tip="Email can not be changed">
+                                <input type="text" value={user.email} className="w-full input cursor-default input-bordered " readOnly />
+                            </div>
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-lg text-black font-bold">Photo</span>
                             </label>
-                            <input type="text" value={photo} onChange={handlePhotoChange} name="photo"  className="input input-bordered" />
+                            <input type="text" value={photo} onChange={handlePhotoChange} name="photo" className="input input-bordered" />
                         </div>
                         <div className="form-control mt-6">
                             <button className="bg-green-500 text-black font-bold p-3 rounded-lg">Update</button>
